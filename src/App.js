bee0892f4f00b6b5892b4a2b5a424299f3c3b6ff
Renier Vargas Homestead <renier1989@gmail.com>
2023-06-26 20:27:19 +0000

    implementing how to use Outlet for nested Routes

--- src/App.js
+++ src/App.js
@@ -15,8 +15,11 @@ function App() {
           
           {/* el Route es el que me desplegara la nueva ruta y el o los componentes que se van a renderizar */}
           <Route path="/" element={<HomePage />} />
-          <Route path="/blog" element={<BlogPage />} />
-          <Route path="/blog/:slug" element={<BlogPost />} />
+          {/* con esto vamos a poder hacer uso del Outlet, quiere decir que el componente BlogPage (el papa) , va a poder renderizar en el mismo lo que tenga el BlogPost en el lugar que llamemos al componente <Outlet /> */}
+          <Route path="/blog" element={<BlogPage />} >
+            <Route path=":slug" element={<BlogPost />} />
+          </Route>
+          
           <Route path="/profile" element={<ProfilePage />} />
           
           <Route path="*" element={<p> Not Found!</p>} />   {/* este me dira que si ingreso a una ruta que no este definida muestro algo opcional importante , esta tiene que estar difinida al final de todas las rutas validas */}
--- src/BlogPage/index.js
+++ src/BlogPage/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {Link , Outlet} from 'react-router-dom';
 import {postdata} from '../Constant/postdata.js'
 function BlogPage() {
   return (
     <div>
+
+      <h1>Blog</h1>
+
+      {/* con esto puedo hacer que las rutas anidadas se muestren dentro  del mimso componente padre .  */}
+      {/* importante en la difinicion de rutas, la ruta debe estar contenida on encerrada en otra etiqueta route para poder hacer usao del Outlet */}
+      <Outlet />   
+
       <ul>
           {postdata.map((post)=>(
             <BlogLink key={post.slug} post={post} />
