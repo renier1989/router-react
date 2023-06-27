193c388200cd4b9d6d31d0cceaa1226d0fd7598d
Renier Vargas Homestead <renier1989@gmail.com>
2023-06-27 15:36:33 +0000

    Progress in validating menu with the AuthContext

--- src/Menu/index.js
+++ src/Menu/index.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
+import { useAuth } from '../Auth/auth'
 
 function Menu() {
+  const auth = useAuth();
   return (
     <nav>
         <ul>
-          {routes.map((route,index)=>(
+          {routes.map((route,index)=>{
+            if(route.private && !auth.user) return null;
+
+            return(
             <li key={index}>
               <NavLink 
               style={({isActive})=>({color : isActive ? 'red' : 'green'})}
@@ -14,7 +19,8 @@ function Menu() {
                 {route.text}
               </NavLink>
             </li>
-          ))
+          )}
+          )
           }
 
         </ul>
@@ -26,22 +32,27 @@ const routes = [];
 routes.push({
   to : '/',
   text : 'Home',
+  private : false,
 });
 routes.push({
   to : '/blog',
   text : 'Blog',
+  private : false,
 });
 routes.push({
   to : '/profile',
   text : 'Profile',
+  private : true,
 });
 routes.push({
   to : '/login',
   text : 'Login',
+  private : false,
 });
 routes.push({
   to : '/logout',
   text : 'Logout',
+  private : true,
 });
   
 export  {Menu}
